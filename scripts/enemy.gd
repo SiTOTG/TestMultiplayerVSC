a469e2c05e045fa81ce385cd88a233d6ee41850a
Zemeio <tonhoanhon@gmail.com>
2022-10-29 07:03:30 +0900

    Add repulsion between enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,9 +2,10 @@ class_name Enemy
 extends Area2D
 
 const SPEED = 150
-
+const AVOID_FORCE = 20
 
 @onready var player_detector: Area2D = $PlayerDetector
+@onready var enemy_detector: Area2D = $EnemyDetector
 @onready var damage_cooldown: Timer = $DamageCooldown
 
 var target: Player
@@ -16,8 +17,19 @@ func _physics_process(delta):
 	if priority_target:
 		var direction = position.direction_to(priority_target.position)
 		velocity = direction * SPEED
+		_avoid_other_enemies()
 		position += velocity * delta
-	
+		
+	_apply_damage_to_players()
+
+func _avoid_other_enemies():
+	for enemy in enemy_detector.get_overlapping_areas():
+		enemy = enemy as Enemy
+		if not enemy or enemy == self:
+			continue
+		velocity = velocity + (global_position - enemy.global_position).normalized()*AVOID_FORCE
+
+func _apply_damage_to_players():
 	if monitoring and has_overlapping_areas():
 		var player_hurtboxes = get_overlapping_areas()
 		for hurtbox in player_hurtboxes:
